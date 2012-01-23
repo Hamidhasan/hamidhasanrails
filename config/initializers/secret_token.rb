6f92eb07730b7e11d88f2e0e11203ec342c81b04
Hamidhasan G. Ahmed <hamidhasan14@gmail.com>
2012-01-23 00:09:22 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hamidhasanrails::Application.config.secret_token = 'afc436324312e31ceaf517af014d46ae5e2496334c02fd69bb58c9a770f9d20e6b5825b74494e16b0928cd9f094fae24971b0856f82ea9ff35b81ca3cc35c580'
