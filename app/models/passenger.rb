a505a30714378e31fa59d850af7236611843ea76
Hamidhasan G. Ahmed <hamidhasan14@gmail.com>
2012-02-25 22:40:36 -0500

    Added some integration tests.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,8 +1,8 @@
 class Passenger < ActiveRecord::Base
   has_many :luggage
   has_many :flight
-  # validates :name, :uniqueness => true
- #  validates :name, :format => { :with => /[a-zA-Z\s]/,
-  #   :message => "Only letters (and spaces) allowed" }
+   validates :name, :uniqueness => true
+   validates :name, :format => { :with => /[a-zA-Z\s]/,
+       :message => " - Only letters (and spaces) allowed" }
   validates :email, :uniqueness => true
 end
---
+++ test/integration/flight_deparr_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class FlightDeparrTest < ActionDispatch::IntegrationTest
+  test "departure_length" do
+    
+     visit flights_path
+     click_link "New Flight"
+     fill_in "Departure", with: "X"
+     fill_in "Arrival", with: "ABC"
+     click_button "Create Flight"
+     
+     error_message = "Departure is too short (minimum is 3 characters)"
+     asset page.has_content?(error_message)
+   end
+   
+  test "arrival_length" do
+    
+     visit flights_path
+     click_link "New Flight"
+     fill_in "Departure", with: "ABC"
+     fill_in "Arrival", with: "X"
+     click_button "Create Flight"
+     
+     error_message = "Arrival is too short (minimum is 3 characters)"
+     asset page.has_content?(error_message)
+   end   
+end
