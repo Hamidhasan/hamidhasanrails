744eac70aa384b7b7cee320634a5a8c73fb758b1
Hamidhasan G. Ahmed <hamidhasan14@gmail.com>
2012-02-25 22:00:40 -0500

    Added some unit tests.

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ end
 
 group :development, :test do
   gem 'sqlite3'
+  gem 'capybara_rails'
 end
 
 group :production do
--- test/unit/passenger_test.rb
+++ test/unit/passenger_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PassengerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "username format" do
+    myPerson = {name: "1234", email: "test@test.com"}
+    passenger = Passenger.new(myPerson)
+    assert passenger.valid?
+  end
+  
+  test "username uniqueness" do
+    myPerson = {name: "hello", email: "test@test.com"}
+    second = {name: "hello", email: "test2@test.com"}
+    passenger = Passenger.new(myPerson)
+    passenger2 = Passenger.new(second)
+    assert passenger2.valid?
+  end
+  
 end
