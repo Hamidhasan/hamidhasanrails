6736ee23525ebaacf4e652022fcce0ae453bdfc5
Hamidhasan G. Ahmed <hamidhasan14@gmail.com>
2012-02-25 19:53:24 -0500

    Added proper datetime selectors.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,7 @@ class Passenger < ActiveRecord::Base
   has_many :luggage
   has_many :flight
   validates :name, :uniqueness => true
+  validates :name, :format => { :with => /[a-zA-Z\s]/,
+    :message => "Only letters (and spaces) allowed" }
   validates :email, :uniqueness => true
 end
