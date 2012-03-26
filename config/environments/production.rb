1669dffc8028b433f3d52c77701fb5739a04e79c
Hamidhasan G. Ahmed <hamidhasan14@gmail.com>
2012-03-25 23:40:45 -0400

    added Sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,15 @@ Hamidhasanrails::Application.configure do
   
   #Need this for Devise.
   config.action_mailer.default_url_options = {:host => 'travelrecorder.herokuapp.com'}
+  
+  #Sendgrid required options.
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :authentication => :plain,
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'heroku.com'
+    }
+    
 end
